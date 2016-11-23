b3a1fcbe625cee3b9f3e0765e6abc287d3d01e68
mayo505 <marek.turis@gmail.com>
2016-11-23 17:23:11 +0100

    Generic DAO created

---
+++ persistance/src/main/java/cz/muni/fi/pa165/dao/CrudDao.java
@@ -0,0 +1,44 @@
+package cz.muni.fi.pa165.dao;
+
+import java.util.List;
+
+/**
+ * @author Marek Turis
+ */
+public interface CrudDao<TEntity> {
+	/**
+	 * Finds all entities
+	 *
+	 * @return all entities
+	 */
+	List<TEntity> findAll();
+
+	/**
+	 * Finds entity by id
+	 *
+	 * @param id Id of entity
+	 * @return found entity
+	 */
+	TEntity findById(int id);
+
+	/**
+	 * Creates new entity
+	 *
+	 * @param entity to create
+	 */
+	void create(TEntity entity);
+
+	/**
+	 * Deletes entity
+	 *
+	 * @param entity
+	 */
+	void delete(TEntity entity);
+
+	/**
+	 * Updates entity
+	 *
+	 * @param entity
+	 */
+	void update(TEntity entity);
+}
--- persistance/src/main/java/cz/muni/fi/pa165/dao/UserDao.java
+++ persistance/src/main/java/cz/muni/fi/pa165/dao/UserDao.java
@@ -9,40 +9,6 @@ import java.util.List;
  *
  * @author Marek Turis
  */
-public interface UserDao {
-	/**
-	 * Finds all users
-	 *
-	 * @return all users
-	 */
-	List<User> findAll();
+public interface UserDao extends CrudDao<User> {
 
-	/**
-	 * Finds user by id
-	 *
-	 * @param id Id of user
-	 * @return found user
-	 */
-	User findById(int id);
-
-	/**
-	 * Creates new user
-	 *
-	 * @param user to create
-	 */
-	void create(User user);
-
-	/**
-	 * Deletes user
-	 *
-	 * @param user
-	 */
-	void delete(User user);
-
-	/**
-	 * Updates user
-	 *
-	 * @param user
-	 */
-	void update(User user);
 }
