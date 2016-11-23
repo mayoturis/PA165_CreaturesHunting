4c001cd6ef56193480ab1455a7982c79d72a2f56
mayo505 <marek.turis@gmail.com>
2016-11-23 17:25:29 +0100

    UserService set to extend generic service

--- service/src/main/java/cz/muni/fi/pa165/service/UserService.java
+++ service/src/main/java/cz/muni/fi/pa165/service/UserService.java
@@ -1,7 +1,9 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.entities.User;
+
 /**
  * @author Marek Turis
  */
-public interface UserService {
+public interface UserService extends CrudService<User> {
 }
--- service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
@@ -1,7 +1,19 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.dao.UserDao;
+import cz.muni.fi.pa165.entities.User;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  * @author Marek Turis
  */
-public class UserServiceImpl {
+@Named
+public class UserServiceImpl extends CrudServiceImpl<User> implements UserService {
+
+	@Inject
+	public UserServiceImpl(UserDao userDao) {
+		super(userDao);
+	}
 }
