ab1bdd7c3f26cf114253c6504b6ea05c6505b3a3
Michael Cada <mcada@redhat.com>
2016-11-25 22:26:50 +0100

    Added MonsterServiceTest and MonsterFacadeTest

--- service/src/test/java/cz/muni/fi/pa165/service/facadeImpl/MonsterFacadeTest.java
+++ service/src/test/java/cz/muni/fi/pa165/service/facadeImpl/MonsterFacadeTest.java
@@ -1,7 +1,119 @@
 package cz.muni.fi.pa165.service.facadeImpl;
 
+import cz.muni.fi.pa165.dto.MonsterDTO;
+import cz.muni.fi.pa165.entities.Monster;
+import cz.muni.fi.pa165.facade.MonsterFacade;
+import cz.muni.fi.pa165.service.services.MonsterService;
+import cz.muni.fi.pa165.service.services.mapping.MappingService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.verification.VerificationMode;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
- * Created by Marek on 22.11.2016.
+ * @author Michael Cada
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MonsterFacadeTest {
+	private static VerificationMode once = times(1);
+
+	private MonsterFacade monsterFacade;
+
+	@Mock
+	private Monster monster;
+	@Mock
+	private MonsterDTO monsterDTO;
+
+	@Mock
+	private MonsterService monsterService;
+
+	@Mock
+	private MappingService mappingService;
+
+	@Before
+	public void setUp() {
+		monsterFacade = new MonsterFacadeImpl(monsterService, mappingService);
+	}
+
+	@Test
+	public void deleteCallsServiceTest() {
+		when(mappingService.map(monsterDTO, Monster.class)).thenReturn(monster);
+
+		monsterFacade.delete(monsterDTO);
+
+		verify(monsterService, once).delete(monster);
+	}
+
+	@Test
+	public void deleteByIdFindsAndDeletesMonsterTest() {
+		int monsterId = 666;
+		when(monsterService.findById(monsterId)).thenReturn(monster);
+
+		monsterFacade.delete(monsterId);
+
+		verify(monsterService, once).delete(monster);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void deleteByIdThrowsExceptionIfNotFoundTest() {
+		int monsterId = 666;
+		when(monsterService.findById(monsterId)).thenReturn(null);
+
+		monsterFacade.delete(monsterId);
+	}
+
+	@Test
+	public void findByIdCallsServiceTest() {
+		int monsterId = 666;
+		when(monsterService.findById(monsterId)).thenReturn(monster);
+		when(mappingService.map(monster, MonsterDTO.class)).thenReturn(monsterDTO);
+
+		MonsterDTO actualMonster = monsterFacade.findById(monsterId);
+
+		verify(monsterService, once).findById(monsterId);
+		Assert.assertEquals(monsterDTO, actualMonster);
+	}
+
+	@Test
+	public void findAllCallsServiceTest() {
+		List<Monster> monsterList = Arrays.asList(monster);
+		List<MonsterDTO> monsterDTOList = Arrays.asList(monsterDTO);
+		when(monsterService.findAll()).thenReturn(monsterList);
+		when(mappingService.map(monsterList, MonsterDTO.class)).thenReturn(monsterDTOList);
+
+		List<MonsterDTO> actualMonsterDTOList = monsterFacade.findAll();
+
+		Assert.assertEquals(monsterDTOList, actualMonsterDTOList);
+	}
+
+	@Test
+	public void createCallsServiceAndReturnsIdTest() {
+		int expectedId = 666;
+		when(mappingService.map(monsterDTO, Monster.class)).thenReturn(monster);
+		when(monsterService.create(monster)).thenReturn(expectedId);
+
+		int actualId = monsterFacade.create(monsterDTO);
+
+		verify(monsterService, once).create(monster);
+		Assert.assertEquals(expectedId, actualId);
+	}
+
+	@Test
+	public void updateCallsServiceTest() {
+		when(mappingService.map(monsterDTO, Monster.class)).thenReturn(monster);
+
+		monsterFacade.update(monsterDTO);
+
+		verify(monsterService, once).update(monster);
+	}
 }
--- service/src/test/java/cz/muni/fi/pa165/service/services/MonsterServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/service/services/MonsterServiceTest.java
@@ -1,7 +1,128 @@
 package cz.muni.fi.pa165.service.services;
 
+import cz.muni.fi.pa165.dao.MonsterDao;
+import cz.muni.fi.pa165.entities.Area;
+import cz.muni.fi.pa165.entities.Monster;
+import cz.muni.fi.pa165.service.exceptions.HuntingPersistenceException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.verification.VerificationMode;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
 /**
- * Created by Marek on 22.11.2016.
+ * Tests for MonsterService
+ *
+ * @author Michael Cada
  */
+@Transactional
+@RunWith(MockitoJUnitRunner.class)
 public class MonsterServiceTest {
+
+	private static VerificationMode once = times(1);
+
+	private MonsterService monsterService;
+
+	@Mock
+	private MonsterDao monsterDao;
+
+	@Mock
+	private Monster monster;
+
+	@Mock
+	private Area area;
+
+	@Before
+	public void setup() {
+		monsterService = new MonsterServiceImpl(monsterDao);
+	}
+
+	@Test
+	public void createTest() {
+
+		Mockito.when(monster.getId()).thenReturn(666);
+		int actual = monsterService.create(monster);
+		Assert.assertEquals(666, actual);
+		Mockito.verify(monsterDao, once).create(monster);
+	}
+
+	@Test
+	public void findByIdCallsDaoTest() {
+		int id = 666;
+		when(monsterDao.findById(id)).thenReturn(monster);
+
+		Monster actualMonster = monsterService.findById(id);
+
+		Assert.assertEquals(monster, actualMonster);
+	}
+
+	@Test(expected = HuntingPersistenceException.class)
+	public void findByIdRuntimeExcTest() {
+		int id = 666;
+		doThrow(RuntimeException.class).when(monsterDao).findById(id);
+
+		monsterService.findById(id);
+	}
+
+	@Test
+	public void findAllCallsDaoTest() {
+		List<Monster> monsterList = Arrays.asList(monster);
+		when(monsterDao.findAll()).thenReturn(monsterList);
+
+		List<Monster> actualMonsterList = monsterService.findAll();
+
+		Assert.assertEquals(monsterList, actualMonsterList);
+	}
+
+	@Test(expected = HuntingPersistenceException.class)
+	public void findAllThrowsPersistanceExcInCaseOfDaoExceptionTest() {
+		doThrow(RuntimeException.class).when(monsterDao).findAll();
+
+		monsterService.findAll();
+	}
+
+	@Test(expected = HuntingPersistenceException.class)
+	public void createWithDatabaseProblemTest() {
+		Mockito.doThrow(RuntimeException.class).when(monsterDao).create(monster);
+
+		monsterService.create(monster);
+	}
+
+	@Test
+	public void updateCallsDaoTest() {
+		monsterService.update(monster);
+
+		verify(monsterDao, once).update(monster);
+	}
+
+	@Test(expected = HuntingPersistenceException.class)
+	public void updateRuntimeExcTest() {
+		doThrow(RuntimeException.class).when(monsterDao).update(monster);
+
+		monsterService.update(monster);
+	}
+
+	@Test
+	public void deleteCallsDaoTest() {
+		monsterService.delete(monster);
+
+		verify(monsterDao, once).delete(monster);
+	}
+
+	@Test(expected = HuntingPersistenceException.class)
+	public void deleteThrowsPersistanceIncaptionInCaseOfDaoExceptionTest() {
+		doThrow(RuntimeException.class).when(monsterDao).delete(monster);
+
+		monsterService.delete(monster);
+	}
+
 }
