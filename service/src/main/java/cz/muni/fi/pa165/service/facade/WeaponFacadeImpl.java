a7ea117c3e9b5ebc8c6f819edee47f9fa6f82342
mayo505 <marek.turis@gmail.com>
2016-11-25 17:09:31 +0100

    Constructor parameter changed

--- service/src/main/java/cz/muni/fi/pa165/service/facade/WeaponFacadeImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/facade/WeaponFacadeImpl.java
@@ -3,8 +3,8 @@ package cz.muni.fi.pa165.service.facade;
 import cz.muni.fi.pa165.dto.WeaponDTO;
 import cz.muni.fi.pa165.entities.Weapon;
 import cz.muni.fi.pa165.facade.WeaponFacade;
-import cz.muni.fi.pa165.service.CrudService;
 import cz.muni.fi.pa165.service.MappingService;
+import cz.muni.fi.pa165.service.WeaponService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -16,7 +16,7 @@ import javax.inject.Named;
 public class WeaponFacadeImpl extends CrudFacadeImpl<WeaponDTO, Weapon> implements WeaponFacade {
 
 	@Inject
-	public WeaponFacadeImpl(CrudService<Weapon> crudService, MappingService mappingService) {
+	public WeaponFacadeImpl(WeaponService crudService, MappingService mappingService) {
 		super(crudService, mappingService, WeaponDTO.class, Weapon.class);
 	}
 }
