b61cda83429892fa3f3b962c1eb0270b4ed88b74
Simcsa <simcsa13@gmail.com>
2016-11-25 17:47:09 +0100

    monster dto and facade created

---
+++ api/src/main/java/cz/muni/fi/pa165/facade/MonsterFacade.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.facade;
+
+import cz.muni.fi.pa165.dto.MonsterDTO;
+
+/**
+ * @author Simona Kruppova
+ */
+public interface MonsterFacade extends CrudFacade<MonsterDTO> {
+}
