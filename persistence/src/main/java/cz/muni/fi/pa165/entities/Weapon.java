1b61bd870184cb4eba02fa7f5917a02afacf8c94
Michael Cada <mcada@redhat.com>
2016-11-25 21:11:33 +0100

    Changing indentation to tabs

--- persistence/src/main/java/cz/muni/fi/pa165/entities/Weapon.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entities/Weapon.java
@@ -20,97 +20,97 @@ import java.util.Set;
 @javax.persistence.Entity
 public class Weapon implements cz.muni.fi.pa165.entities.base.Entity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    @NotNull
-    @Column(nullable = false, unique = true)
-    private String name;
+	@NotNull
+	@Column(nullable = false, unique = true)
+	private String name;
 
-    @Min(0)
-    private int range;
+	@Min(0)
+	private int range;
 
-    @NotNull
-    private Ammunition ammunition;
+	@NotNull
+	private Ammunition ammunition;
 
-    @ManyToMany(mappedBy = "weapons")
-    private Set<User> users = new HashSet<User>();
+	@ManyToMany(mappedBy = "weapons")
+	private Set<User> users = new HashSet<User>();
 
-    @ManyToMany(mappedBy = "weapons")
-    private Set<Monster> monsters = new HashSet<Monster>();
+	@ManyToMany(mappedBy = "weapons")
+	private Set<Monster> monsters = new HashSet<Monster>();
 
-    protected Weapon() {
-        // required by Hibernate
-    }
+	protected Weapon() {
+		// required by Hibernate
+	}
 
-    public Weapon(String name, Ammunition ammunition) {
-        this.name = name;
-        this.ammunition = ammunition;
-    }
+	public Weapon(String name, Ammunition ammunition) {
+		this.name = name;
+		this.ammunition = ammunition;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Weapon)) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Weapon)) return false;
 
-        Weapon weapon = (Weapon) o;
+		Weapon weapon = (Weapon) o;
 
-        return getName().equals(weapon.getName());
-    }
+		return getName().equals(weapon.getName());
+	}
 
-    @Override
-    public int hashCode() {
-        return getName().hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return getName().hashCode();
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getRange() {
-        return range;
-    }
+	public int getRange() {
+		return range;
+	}
 
-    public void setRange(int range) {
-        this.range = range;
-    }
+	public void setRange(int range) {
+		this.range = range;
+	}
 
-    public Set<User> getUsers() {
-        return users;
-    }
+	public Set<User> getUsers() {
+		return users;
+	}
 
-    public Set<Monster> getMonsters() {
-        return monsters;
-    }
+	public Set<Monster> getMonsters() {
+		return monsters;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public Ammunition getAmmunition() {
-        return ammunition;
-    }
+	public Ammunition getAmmunition() {
+		return ammunition;
+	}
 
-    public void setAmmunition(Ammunition ammunition) {
-        this.ammunition = ammunition;
-    }
+	public void setAmmunition(Ammunition ammunition) {
+		this.ammunition = ammunition;
+	}
 
-    public void addMonsters(List<Monster> monsters) {
-        this.monsters.addAll(monsters);
-    }
+	public void addMonsters(List<Monster> monsters) {
+		this.monsters.addAll(monsters);
+	}
 
-    public void addMonster(Monster monster) {
-        this.monsters.add(monster);
-        monster.addWeapon(this);
-    }
+	public void addMonster(Monster monster) {
+		this.monsters.add(monster);
+		monster.addWeapon(this);
+	}
 
-    public void addUser(User user) {
-        this.users.add(user);
-        user.addWeapon(this);
-    }
+	public void addUser(User user) {
+		this.users.add(user);
+		user.addWeapon(this);
+	}
 }
